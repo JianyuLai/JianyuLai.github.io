06638d70c8baea862823700f5aa4226ee609cd3a
Jianyu Lai <jianyulie@hotmail.com>
2026-03-30 00:09:39 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://jianyulai.github.io',
-  base: '/academic-website/',
-
   integrations: [
     tailwind({
       applyBaseStyles: false,
